c66b677d97b10cb7f39b030643fca3ac9c2532c4
姚昕辰 <hubert@yaoxinchendeMacBook-Air.local>
2024-04-22 15:34:54 +0800

    Rustlings: Accomplish until Algorithm!

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let parts : Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Person::default();
+        }
+
+        let mut name = String::new();
+        if parts[0].is_empty() {
+            return Person::default();
+        }else {
+            name = String::from(parts[0]);
+        }
+
+        let num = parts[1].parse::<usize>();
+
+        match num {
+            Ok(age) => Person {name, age},
+            Err(_) => Person::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(Self::Err::Empty);
+        }
+
+        let parts : Vec<&str> = s.split(',').collect();
+
+        if parts.len() != 2 {
+            return Err(Self::Err::BadLen);
+        }
+
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            return Err(Self::Err::NoName);
+        }
+
+        let age = match parts[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(err) => return Err(ParsePersonError::ParseInt(err)),
+        };
+
+        Ok(Person{name, age})
     }
 }
 
